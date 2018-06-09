2f38a0ee0f3238d2d7cc90e385a11618e4d0989d
Jan Bölsche <regular.gonzales@googlemail.com>
2018-06-09 10:43:34 +0000

    Copy from rfm12b-linux/example, set group explicitly, hack to test sending binary data

---
+++ common/common.h
@@ -0,0 +1,32 @@
+/* rfm12b-linux: linux kernel driver for the rfm12(b) RF module by HopeRF
+*  Copyright (C) 2013 Georg Kaindl
+*  
+*  This file is part of rfm12b-linux.
+*  
+*  rfm12b-linux is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 2 of the License, or
+*  (at your option) any later version.
+*  
+*  rfm12b-linux is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*  
+*  You should have received a copy of the GNU General Public License
+*  along with rfm12b-linux.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#if defined(__COMMON_H__)
+#define __COMMON_H__
+
+// get rfm12b stats via ioctl and print to stdout
+void print_stats(int fd);
+
+// return 0 if module doesn't support rssi, not-0 otherwise
+int supports_rssi(int fd);
+
+// print the current rssi reading
+void print_rssi(int fd);
+
+#endif // __COMMON_H__
